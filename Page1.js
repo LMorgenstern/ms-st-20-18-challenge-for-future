17a56f81479a8d845c97aa3e9297c012b3413679
LMorgenstern <66073748+LMorgenstern@users.noreply.github.com>
2020-08-14 13:04:34 +0200

    jhn

--- Page1.js
+++ Page1.js
@@ -23,3 +23,14 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+var firebaseConfig = {
+  apiKey: " api-key ",
+  authDomain: " project-id .firebaseapp.com",
+  databaseURL: "https:// project-id .firebaseio.com",
+  projectId: " project-id ",
+  storageBucket: " project-id .appspot.com",
+  messagingSenderId: " sender-id ",
+  appId: " app-id ",
+  measurementId: "G- measurement-id ",
+};
