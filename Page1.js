6e7bacf9181c88c68c94c1c546aee7f1baec20a1
LMorgenstern <66073748+LMorgenstern@users.noreply.github.com>
2020-08-25 14:22:24 +0200

    fghj

--- Page1.js
+++ Page1.js
@@ -1,31 +1,31 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+var count = 200;
+var defaults = {
+  origin: { y: 0.7 }
+};
 
-// Get the button that opens the modal
-var btn = document.getElementById("btn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+function fire(particleRatio, opts) {
+  confetti(Object.assign({}, defaults, opts, {
+    particleCount: Math.floor(count * particleRatio)
+  }));
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-.showdouble { position: relative;  }
-.singlefade {opacity: 1; transition: opacity 1s; }
-.showdouble .overlay { position: absolute;  }
-.showdouble .overbox { position: absolute; }
-.milky { opacity: 0.5; }
+fire(0.25, {
+  spread: 26,
+  startVelocity: 55,
+});
+fire(0.2, {
+  spread: 60,
+});
+fire(0.35, {
+  spread: 100,
+  decay: 0.91,
+});
+fire(0.1, {
+  spread: 120,
+  startVelocity: 25,
+  decay: 0.92,
+});
+fire(0.1, {
+  spread: 120,
+  startVelocity: 45,
+});
